b09313e1b987f96a81c2f24a4c4f0ec1a3574092
Arti Mane <Apple@Admins-iMac-4.local>
2021-01-01 14:50:56 +0530

    addded newFile

---
+++ NewPracticeFile/newFile.swift
@@ -0,0 +1,18 @@
+//
+//  newFile.swift
+//  NewPracticeFile
+//
+//  Created by Arti Mane on 01/01/21.
+//
+
+import Foundation
+
+class newFile {
+    
+    func new() {
+        print("to add new file")
+    }
+   
+}
+
+
